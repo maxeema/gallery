8c8ab8efe60dc500b55046dfb49f905ce2e8fc36
Maxeem <jmax.posh@gmail.com>
2019-10-21 16:42:24 +0300

    ask server for 30 photos per page
    there is no need to ask 60 / two pages at once

--- lib/conf.dart
+++ lib/conf.dart
@@ -3,14 +3,13 @@
 /// Conf & Const
 ///
 
-const photosPerUiOperation = 60;
+const photosPerUiOperation = 30; // If ask more than apiPhotosPerPage
+                                 //  api will make two or more requests to server
 
 const apiBaseUrl = 'api.unsplash.com';
-const apiPhotosPerPage = 30;
+const apiPhotosPerPage = 30; // now 30 is api restricted max photos per page
 const apiTokens = {
   '896d4f52c589547b2134bd75ed48742db637fa51810b49b607e37e46ab2c0043',
   'ab3411e4ac868c2646c0ed488dfd919ef612b04c264f3374c97fff98ed253dc9',
   'cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0',
 };
-
-
