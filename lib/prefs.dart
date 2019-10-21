8b1813b333b9431591901fad046980b46f21ed99
Maxeem <jmax.posh@gmail.com>
2019-10-22 01:10:57 +0300

    define proper return type

--- lib/prefs.dart
+++ lib/prefs.dart
@@ -1,12 +1,14 @@
 
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'categories.dart';
+
 const _keyCategory = "category";
 
-get prefs => SharedPreferences.getInstance();
+Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
 
 Future<String> loadCategory () async
     => (await prefs).getString(_keyCategory);
 
-Future<void> saveCategory(String category) async
-    => (await prefs).setString(_keyCategory, category);
+Future<void> saveCategory(Category category) async
+    => (await prefs).setString(_keyCategory, categoryToName(category));
