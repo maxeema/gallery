61aa42b6f460d7a11ae835d4846542081c8cf4d9
Maxeem <jmax.posh@gmail.com>
2020-07-12 03:54:22 +0300

    fix about dialog showing on web, linux also

--- lib/ui/about_dialog.dart
+++ lib/ui/about_dialog.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:maxeem_gallery/misc/conf.dart' as conf;
@@ -6,64 +7,81 @@ import 'package:maxeem_gallery/localizations/localization.dart';
 import 'package:maxeem_gallery/misc/util.dart';
 import 'package:package_info/package_info.dart';
 
-showAbout(BuildContext context) {
+showAbout(BuildContext context) async {
 
   final localizations = AppLocalizations.of(context);
+  final platform = Theme.of(context).platform;
 
-  PackageInfo.fromPlatform().then((PackageInfo info) {
-    showAboutDialog(context: context,
-        applicationIcon: FlutterLogo(
-          size: 48, colors: Colors.indigo,
+  //
+  String appName;
+  String appVersion;
+
+  PackageInfo packageInfo;
+  try {
+    packageInfo = await PackageInfo.fromPlatform();
+    appName = packageInfo.appName;
+    appVersion = packageInfo.version;
+  } catch (e) {
+    appName = localizations.appTitle;
+    appVersion = kIsWeb ? "Web"
+        : () {
+      var name = platform.toString();
+      name = name.substring(platform.toString().lastIndexOf(".")+1);
+      return name.substring(0, 1).toUpperCase() + name.substring(1);
+    }();
+  }
+
+  showAboutDialog(context: context,
+    applicationIcon: FlutterLogo(size: 48, colors: Colors.indigo,),
+    applicationName: appName,
+    applicationVersion: appVersion,
+    applicationLegalese: conf.appLegalese,
+    children: [
+      SizedBox(height: 8,),
+      RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+              style: Theme.of(context).textTheme.body1,
+              children: _spanzize(
+                  localizations.appGitHub, 'GitHub',
+                  (nonMatched) => TextSpan(text: nonMatched),
+                  (matched) => TextSpan(
+                    text: matched,
+                    style: TextStyle(color: Theme.of(context).accentColor),
+                    recognizer: TapGestureRecognizer()..onTap = () => launchUrl(conf.appGitHubPage)
+                  )
+              ),
+          )
+      ),
+      if (!kIsWeb && platform == TargetPlatform.android)
+        Container(
+            margin: EdgeInsets.symmetric(vertical: 8),
+            alignment: Alignment.center,
+            child: OutlineButton(
+              textTheme: ButtonTextTheme.primary,
+              child: Text(localizations.seeOnPlay),
+              onPressed: () {
+                launchUrl('https://play.google.com/store/apps/details?id=${packageInfo.packageName}');
+              },
+            )
         ),
-        applicationName: info.appName,
-        applicationVersion: info.version,
-        applicationLegalese: conf.appLegalese,
-        children: [
-          SizedBox(height: 8,),
-          RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                  style: Theme.of(context).textTheme.body1,
-                  children: _spanzize(
-                      localizations.appGitHub, 'GitHub',
-                      (nonMatched) => TextSpan(text: nonMatched),
-                      (matched) => TextSpan(
-                        text: matched,
-                        style: TextStyle(color: Theme.of(context).accentColor),
-                        recognizer: TapGestureRecognizer()..onTap = () => launchUrl(conf.appGitHubPage)
-                      )
-                  ),
-              )
-          ),
-          Container(
-              margin: EdgeInsets.symmetric(vertical: 8),
-              alignment: Alignment.center,
-              child: OutlineButton(
-                textTheme: ButtonTextTheme.primary,
-                child: Text(localizations.seeOnPlay),
-                onPressed: () {
-                  launchUrl('https://play.google.com/store/apps/details?id=${info.packageName}');
-                },
-              )
-          ),
-          RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                style: Theme.of(context).textTheme.body1,
-                children: _spanzize(
-                    localizations.appCredits, 'Unsplash',
-                        (nonMatched) => TextSpan(text: nonMatched),
-                        (matched) => TextSpan(
-                        text: matched,
-                        style: TextStyle(color: Theme.of(context).accentColor),
-                        recognizer: TapGestureRecognizer()..onTap = () => launchUrl('https://unsplash.com')
-                    )
-                ),
-              )
-          ),
-        ]
-    );
-  });
+      RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: Theme.of(context).textTheme.body1,
+            children: _spanzize(
+                localizations.appCredits, 'Unsplash',
+                    (nonMatched) => TextSpan(text: nonMatched),
+                    (matched) => TextSpan(
+                    text: matched,
+                    style: TextStyle(color: Theme.of(context).accentColor),
+                    recognizer: TapGestureRecognizer()..onTap = () => launchUrl('https://unsplash.com')
+                )
+            ),
+          )
+      ),
+    ]
+  );
 }
 
 List<TextSpan>
