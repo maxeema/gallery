4b07079dfe1cfc5f6fbb11b9fb771247b6b27822
Maxeem <jmax.posh@gmail.com>
2020-07-13 03:51:05 +0300

    use 'photo_view' package for great full-size photo viewing experience

--- lib/ui/photo_screen.dart
+++ lib/ui/photo_screen.dart
@@ -1,10 +1,11 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:maxeem_gallery/domain/photo.dart';
 import 'package:maxeem_gallery/misc/util.dart';
-import 'package:transparent_image/transparent_image.dart';
+import 'package:photo_view/photo_view.dart';
 
 class PhotoScreen extends StatelessWidget {
 
@@ -18,20 +19,21 @@ class PhotoScreen extends StatelessWidget {
     final media = MediaQuery.of(context);
     var width = media.size.width;
     var size = Size(width, photo.size.height / (photo.size.width / width));
-    var scrollAxis = Axis.vertical;
-//    print('initial size: $size');
-    if (size.height < media.size.height) {
-      final height = media.size.height;
-      width = photo.size.width / (photo.size.height / height);
-      size = Size(width, height);
-      scrollAxis = Axis.horizontal;
-//      print('updated size: $size');
-    }
+//    var scrollAxis = Axis.vertical;
+////    print('initial size: $size');
+//    if (size.height < media.size.height) {
+//      final height = media.size.height;
+//      width = photo.size.width / (photo.size.height / height);
+//      size = Size(width, height);
+//      scrollAxis = Axis.horizontal;
+////      print('updated size: $size');
+//    }
     final fullSizedUrl = preparePhotoUrl(window, size, photo.url);
 //    print('$fullSizedUrl \n for $size');
+    final bgColor = Color(int.tryParse('FF${photo.color.substring(1)}', radix: 16) ?? Colors.transparent);
     return Scaffold(
       key: Key("photo_scaffold"),
-      backgroundColor: Color(int.tryParse('FF${photo.color.substring(1)}', radix: 16) ?? Colors.transparent),
+      backgroundColor: bgColor,
       //used for UI tests
       floatingActionButton: Opacity(
         opacity: 0,
@@ -45,29 +47,45 @@ class PhotoScreen extends StatelessWidget {
       ),
       body: Hero(
         tag: photo.id,
-        child: SingleChildScrollView(
-          key: Key("photo_scroll"),
-          scrollDirection: scrollAxis,
-          controller: ScrollController(
-            initialScrollOffset: scrollAxis == Axis.vertical ? (size.height-media.size.height) / 2 : (size.width-media.size.width) / 2,
-          ),
-          child: Stack(
-            children: <Widget>[
-              Image.network(
-                url,
-                fit: BoxFit.cover,
-                width: size.width,
-                height: size.height,
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Image.network(
+              url,
+              fit: BoxFit.cover,
+              width: size.width,
+              height: size.height,
+            ),
+            PhotoView(
+              imageProvider: NetworkImage(fullSizedUrl),
+              backgroundDecoration: BoxDecoration(color: bgColor),
+              loadingBuilder: (context, progress) => Center(
+                child: Container(
+                  width: 20.0,
+                  height: 20.0,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 1.5,
+                    value: progress == null
+                        ? null
+                        : progress.cumulativeBytesLoaded /
+                        progress.expectedTotalBytes,
+                  ),
+                ),
               ),
-              FadeInImage.memoryNetwork(
-                image: fullSizedUrl,
-                placeholder: kTransparentImage,
-                fit: BoxFit.cover,
-                width: size.width,
-                height: size.height,
-              ),
-            ],
-          )
+              enableRotation: true,
+              gaplessPlayback: false,
+              initialScale: PhotoViewComputedScale.covered,
+              minScale: PhotoViewComputedScale.contained * 1,
+              maxScale: PhotoViewComputedScale.covered * 2.5,
+            ),
+//              FadeInImage.memoryNetwork(
+//                image: fullSizedUrl,
+//                placeholder: kTransparentImage,
+//                fit: BoxFit.cover,
+//                width: size.width,
+//                height: size.height,
+//              ),
+          ],
         )
       ),
     );
