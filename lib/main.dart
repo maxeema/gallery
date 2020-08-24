32629ebf242cbaaf1fd542e7f86f85b5bb24971a
Maxeem <jmax.posh@gmail.com>
2020-08-24 07:06:54 +0300

    disable unused cuz works in full screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'localizations/localization.dart';
 import 'misc/conf.dart' as conf;
 import 'misc/injection.dart';
-import 'misc/style.dart';
 import 'network/api.dart' as net;
 import 'repository/photos_repository.dart';
 import 'repository/photos_repository_impl.dart';
@@ -26,10 +25,10 @@ class App extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    useEffect(() {
-      UiOverlayStyle.darkStatusBarIcons.apply();
-      return null;
-    }, ["onetime"]);
+//    useEffect(() {
+//      UiOverlayStyle.darkStatusBarIcons.apply();
+//      return null;
+//    }, ["onetime"]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: GalleryScreen(),
